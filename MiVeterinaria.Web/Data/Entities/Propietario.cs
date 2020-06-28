5de137f4fd1ab025d9b6012f75f18c81dec44283
EmiD09 <40042118+EmiD09@users.noreply.github.com>
2020-06-28 17:43:26 -0300

    Parte 1 creada

---
+++ MiVeterinaria.Web/Data/Entities/Propietario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiVeterinaria.Web.Data.Entities
+{
+    public class Propietario
+    {
+        public int id { get; set; }
+        [Display(Name = "Documento")]
+        [MaxLength(30, ErrorMessage = "El {0} campo no puede tener más de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string documento { get; set; }
+        [Display(Name = "Nombre")]
+        [MaxLength(50, ErrorMessage = "El {0} campo no puede tener más de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string nombre { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(50, ErrorMessage = "El {0} campo no puede tener más de {1} caracteres.")]
+        [Display(Name = "Apellido")]
+        public string apellido { get; set; }
+        [MaxLength(20, ErrorMessage = "El {0} campo no puede tener más de {1} caracteres.")]
+        [Display(Name = "Teléfono Fijo")]
+        public string telFijo { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(20, ErrorMessage = "El {0} campo no puede tener más de {1} caracteres.")]
+        [Display(Name = "Teléfono Celular")]
+        public string telCelular { get; set; }
+        [MaxLength(100, ErrorMessage = "El {0} campo no puede tener más de {1} caracteres.")]
+        [Display(Name = "Dirección")]
+        public string direccion { get; set; }
+
+        [Display(Name = "Propietario")]
+        public string NombreApellido => $"{nombre} {apellido}";
+        [Display(Name = "Propietario")]
+        public string NombreApellidoConDire => $"{nombre} {apellido} - {direccion}";
+    }
+}
